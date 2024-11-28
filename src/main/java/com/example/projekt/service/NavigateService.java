62fb239772ffb901fe8c633cc391869b5ac5327b
LegionAFG <badr.said.emil@hotmail.com>
2024-11-28 08:04:55 +0100

    upadete NavigateService

--- src/main/java/com/example/projekt/service/NavigateService.java
+++ src/main/java/com/example/projekt/service/NavigateService.java
@@ -1,7 +1,5 @@
 package com.example.projekt.service;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -16,7 +14,7 @@ public class NavigateService {
     private static final String CLIENT = "/com/example/projekt/Client.fxml";
     private static final String HISTORIES = "/com/example/projekt/Histories.fxml";
 
-    public EventHandler<ActionEvent> navigate(Stage stage, String page) throws IOException {
+    public void navigate(Stage stage, String page) throws IOException {
 
         String fxmlPath = switch (page) {
             case "home" -> HOME;
@@ -32,6 +30,6 @@ public class NavigateService {
         stage.setTitle("Case Pilot");
         stage.setScene(scene);
         stage.show();
-        return null;
+
     }
 }
